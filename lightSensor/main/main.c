7056bf39d89419cddb1dae7d071a5720d90c8666
Fauzaan <fauzaan.mm@gmail.com>
2024-04-06 15:22:23 +0800

    feat: Logger setup

--- lightSensor/main/main.c
+++ lightSensor/main/main.c
@@ -12,8 +12,9 @@
 #include "ble.h"
 #include "http.h"
 #include "globals.h"
+#include "logger.h"
 
-#define LOG_FILE_PATH "/storage/light.txt"
+#define LOG_FILE_PATH "/logs/runtime.txt"
 #define ADC_CHANNEL ADC1_CHANNEL_0 // ADC channel connected to light sensor
 
 #define LIGHT_LEVELS 3
@@ -95,10 +96,12 @@ int log_light_level()
 
 void app_main(void)
 {
+    logger_initFileLogger(LOG_FILE_PATH, 1024 * 1024, 5);
+    logger_setLevel(LogLevel_DEBUG);
     nvs_flash_init();
     wifi_connection();
     vTaskDelay(7000 / portTICK_PERIOD_MS);
-    printf("WIFI was initiated ...........\n\n");
+    LOG_INFO("WIFI was initiated ...........\n\n");
 
     ble_connection();
 
@@ -113,20 +116,20 @@ void app_main(void)
     {
         // Gets the light level (0-4081)
         int light_level = log_light_level();
-        printf("%d\n", light_level);
+        LOG_INFO("%d\n", light_level);
 
         // insert code to query current switch position
         get_switch_position();
         vTaskDelay(2000 / portTICK_PERIOD_MS);
-        printf("PREV_SWITCH_POS: %f\n", PREV_SWITCH_POSITION);
-        printf("CURR_SWITCH_POS: %f\n", CURR_SWITCH_POSITION);
+        LOG_INFO("PREV_SWITCH_POS: %f\n", PREV_SWITCH_POSITION);
+        LOG_INFO("CURR_SWITCH_POS: %f\n", CURR_SWITCH_POSITION);
 
         // Occupancy boolean stored in OCCUPIED global variable
-        printf("OCCUPIED: %d\n", OCCUPIED);
+        LOG_INFO("OCCUPIED: %d\n", OCCUPIED);
 
         // insert code to send optimal action to lights
         float optimal_action = get_optimal_action(light_level, OCCUPIED, (int)CURR_SWITCH_POSITION * 2);
-        printf("Optimal Action: %f\n", optimal_action);
+        LOG_INFO("Optimal Action: %f\n", optimal_action);
 
         // // insert code to send optimal action
         execute_optimal_action(CURR_SWITCH_POSITION, optimal_action);
